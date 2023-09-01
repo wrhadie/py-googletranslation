a7a98901b0f6832239a0d2f65c6baa7d16dd401e
wrhadie <wrhadie@gmail.com>
2023-09-01 22:02:06 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def get_description():
     return description
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 # def get_readme():
